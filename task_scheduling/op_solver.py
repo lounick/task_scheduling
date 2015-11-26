e782da64e7a3650687da2931469488d93efe8c80
Nick Tsiogkas <nt95@hw.ac.uk>
2015-11-26 13:27:19 +0000

    Changed to use sets. Fixed callback.

--- task_scheduling/op_solver.py
+++ task_scheduling/op_solver.py
@@ -53,31 +53,33 @@ from gurobipy import *
 
 def _callback(model, where):
     if where == GRB.callback.MIPSOL:
-        n = model._n
+        V = set(range(model._n))
+        idx_start = model._idxStart
+        idx_finish = model._idxFinish
 
-        solmat = np.zeros((n, n))
+        solmat = np.zeros((model._n, model._n))
         selected = []
 
-        for i in range(n):
-            sol = model.cbGetSolution([model._eVars[i,j] for j in range(n)])
-            selected += [(i, j) for j in range(n) if sol[j] > 0.5]
+        for i in V:
+            sol = model.cbGetSolution([model._eVars[i, j] for j in V])
+            selected += [(i, j) for j in V if sol[j] > 0.5]
 
             solmat[i, :] = sol
 
         if len(selected) <= 1:
             return
 
-        for k in range(1, len(selected)):
+        for k in range(len(selected)):
             el = selected[k]
-            entry = el[0]
+            entry = {el[0]}
+            if len(idx_start.intersection(entry)) == 0:
+                expr1 = quicksum(model._eVars[i, entry.intersection().pop()] for i in V)
+                expr2 = quicksum(model._eVars[entry.intersection().pop(), j] for j in V)
 
-            expr1 = quicksum(model._eVars[i, entry] for i in range(n))
-            expr2 = quicksum(model._eVars[entry, j] for j in range(n))
-
-            model.cbLazy(expr1, GRB.EQUAL, expr2)
+                model.cbLazy(expr1, GRB.EQUAL, expr2)
 
 
-def op_problem(cost, profit, cost_max, idx_start=None, idx_finish=None, **kwargs):
+def op_problem(cost, profit=None, cost_max=np.inf, idx_start=None, idx_finish=None, **kwargs):
     """
     Cost constrained traveling salesman problem solver for a single vehicle using the Gurobi MILP optimiser.
 
@@ -93,99 +95,113 @@ def op_problem(cost, profit, cost_max, idx_start=None, idx_finish=None, **kwargs
     n = cost.shape[0]
 
     if idx_start is None:
-        idx_start = 0
+        idx_start = {0}
+    else:
+        idx_start = {idx_start}
 
     if idx_finish is None:
-        idx_finish = n - 1
+        idx_finish = {n - 1}
+    else:
+        idx_finish = {idx_finish}
+
+    if profit is None:
+        profit = np.ones(n)
+
+    # Create the sets
+
+    V = set(range(n))
 
     m = Model()
 
     # Create model variables
     e_vars = {}
-    for i in range(n):
-        for j in range(n):
+    for i in V:
+        for j in V:
             e_vars[i, j] = m.addVar(vtype=GRB.BINARY, name='e_' + str(i) + '_' + str(j))
     m.update()
 
-    for i in range(n):
+    for i in V:
         e_vars[i, i].ub = 0
     m.update()
 
     u_vars = {}
-    for i in range(n):
+    for i in V:
         u_vars[i] = m.addVar(vtype=GRB.INTEGER, name='u_' + str(i))
     m.update()
 
     # Set objective function (0)
     expr = 0
-    for i in range(1, n-1):
-        for j in range(1, n):
+    for i in V:
+        for j in V:
             expr += profit[i] * e_vars[i, j]
     m.setObjective(expr, GRB.MAXIMIZE)
     m.update()
 
     # Constraints
+
+    # Add constraints for the initial and final node (1)
     # None enters the starting point
-    m.addConstr(quicksum(e_vars[j, idx_start] for j in range(n)) == 0)
+    m.addConstr(quicksum(e_vars[j, idx_start.intersection().pop()] for j in V.difference(idx_start)) == 0, "s_entry")
     m.update()
 
     # None exits the finish point
-    m.addConstr(quicksum(e_vars[idx_finish, j] for j in range(n)) == 0)
+    m.addConstr(quicksum(e_vars[idx_finish.intersection().pop(), j] for j in V.difference(idx_finish)) == 0, "f_exit")
     m.update()
 
-    # From all other points someone exits
-    for i in range(n):
-        if i != idx_finish:
-            m.addConstr(quicksum(e_vars[i, j] for j in range(n) if i != j) <= 1)
+    # Always exit the starting point
+    m.addConstr(quicksum(e_vars[idx_start.intersection().pop(), i] for i in V.difference(idx_start)) == 1, "s_exit")
     m.update()
 
-    # To all other points someone enters
-    for i in range(n):
-        if i != idx_start:
-            m.addConstr(quicksum(e_vars[j, i] for j in range(n) if i != j) <= 1)
+    # Always enter the finish point
+    m.addConstr(quicksum(e_vars[i, idx_finish.intersection().pop()] for i in V.difference(idx_finish)) == 1, "f_entry")
     m.update()
 
-    # Add constraints for the initial and final node (1)
-    m.addConstr(quicksum(e_vars[idx_start, i] for i in range(1, n)) == 1)
-    m.addConstr(quicksum(e_vars[i, idx_finish] for i in range(n - 1)) == 1)
+    # From all other points someone may exit
+    for i in V.difference(idx_start, idx_finish):
+        m.addConstr(quicksum(e_vars[i, j] for j in V if i != j) <= 1, "v_" + str(i) + "_exit")
     m.update()
 
-    # # Add constraints for other nodes (2)
-    # for k in range(1, n - 1):
-    #     m.addConstr(quicksum(e_vars[i, k] for i in range(n - 1)) <= 1)
-    #     m.addConstr(quicksum(e_vars[k, j] for j in range(1, n)) <= 1)
+    # To all other points someone may enter
+    for i in V.difference(idx_start, idx_finish):
+        m.addConstr(quicksum(e_vars[j, i] for j in V if i != j) <= 1, "v_" + str(i) + "_entry")
+    m.update()
+
+    # If someone enters a point (not the start a finish one) he must exit
+    # for k in V.difference(idx_start, idx_finish):
+    #     expr0 = quicksum(e_vars[i, k] for i in V.difference(idx_finish))
+    #     expr1 = quicksum(e_vars[k, i] for i in V.difference(idx_start))
+    #     m.addConstr(expr0, GRB.EQUAL, expr1, "v_" + str(k) + "_cardinality")
+    # m.update()
 
     # Add cost constraints (3)
     expr = 0
-    for i in range(n):
-        for j in range(n):
-            if i != j:
-                expr += cost[i, j] * e_vars[i, j]
-    m.addConstr(expr <= cost_max)
+    for i in V:
+        for j in V:
+            expr += cost[i, j] * e_vars[i, j]
+    m.addConstr(expr <= cost_max, "max_energy")
     m.update()
 
     # Constraint (4)
-    u_vars[idx_start].lb = 0
-    u_vars[idx_start].ub = 0
-
-    for i in range(1, n):
-        u_vars[i].lb = 1
+    for i in V:
+        u_vars[i].lb = 0
         u_vars[i].ub = n
     m.update()
 
     # Add subtour constraint (5)
-    for i in range(1, n):
-        for j in range(1, n):
-            if i != j:
-                m.addConstr(u_vars[i] - u_vars[j] + 1, GRB.LESS_EQUAL, (n - 1)*(1 - e_vars[i, j]))
+    for i in V:
+        for j in V:
+            m.addConstr(u_vars[i] - u_vars[j] + 1, GRB.LESS_EQUAL, (n - 1)*(1 - e_vars[i, j]),
+                        "sec_" + str(i) + "_" + str(j))
     m.update()
 
     m._n = n
     m._eVars = e_vars
     m._uVars = u_vars
+    m._idxStart = idx_start
+    m._idxFinish = idx_finish
     m.update()
 
-    m.params.OutputFlag = 0
+    m.params.OutputFlag = 1
     m.params.LazyConstraints = 1
     m.optimize(_callback)
 
@@ -193,9 +209,9 @@ def op_problem(cost, profit, cost_max, idx_start=None, idx_finish=None, **kwargs
     u = m.getAttr('X', u_vars)
     selected = [(i, j) for i in range(n) for j in range(n) if solution[i, j] > 0.5]
 
-    solmat = np.zeros((n, n))
-    for k, v in solution.iteritems():
-        solmat[k[0], k[1]] = v
+    # solmat = np.zeros((n, n))
+    # for k, v in solution.iteritems():
+    #     solmat[k[0], k[1]] = v
 
     # print("\n")
     # print(solmat)
@@ -204,7 +220,7 @@ def op_problem(cost, profit, cost_max, idx_start=None, idx_finish=None, **kwargs
     # print(sum(cost[s[0], s[1]] for s in selected))
 
     route = []
-    next_city = idx_start
+    next_city = idx_start.intersection().pop()
 
     while len(selected) > 0:
         for i in range(len(selected)):
@@ -220,11 +236,12 @@ def op_problem(cost, profit, cost_max, idx_start=None, idx_finish=None, **kwargs
 
 
 def main():
+
     import time
     RANDOM = True
     if RANDOM:
         # generate random problem
-        n = 10
+        n = 30
         np.random.seed(42)
         points = np.random.randint(-50, 50, (n, 2))
         profits = np.ones(n)
@@ -250,16 +267,16 @@ def main():
     # Divide distances by maximum speed. To get time approximation.
     # distances = distances / 0.8
 
-    #print(distances)
+    print(distances)
 
     # solve using the Gurobi solver
     st = time.time()
 
-    max_cost = distances[0, -1]
-    max_cost = 340
+    max_cost = distances[1, 4]
+    max_cost = 200
     print('Max Cost: %s' % max_cost)
 
-    tsp_route, total_cost, model = op_problem(distances, profits, max_cost)
+    tsp_route, total_cost, model = op_problem(distances, profits, max_cost, 1, 4)
 
 
     dt = time.time() - st
